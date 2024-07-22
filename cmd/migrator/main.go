b1135a51e5416af77317ea6ce3176883173edbeb
AmirBazanov <flash5651311@gmail.com>
2024-07-22 17:12:33 +0300

    little fix

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	_ "github.com/golang-migrate/migrate/v4"
+	migrate "github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
